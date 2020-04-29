2c1fa81f2b9da63c243cbd51c1412299ecaeb86b
spacehaz <spacehaz@gmail.com>
2020-04-29 14:29:21 +0300

    added fix

--- modules/find-next-question.js
+++ modules/find-next-question.js
@@ -11,21 +11,32 @@ const findNextQuestion = ({ answers }) => {
 
 const checkAnswerValidity = ({ answer, answers }) => {
   const { condition } = answer
+  console.log({ answer, answers })
   if (!condition) { return answer }
+  // одновременно должно выполняться
   const conditionParsed = condition.split('/')
   for (let x = 0; x < conditionParsed.length; x++) {
+    // "2__6||8||9" => [2, 6||8||9]
+    // "2__6" => [2, 6]
     const singleAnswerCondition = conditionParsed[x].split('__')
+    // если только один ответ подразумевается
     if (singleAnswerCondition[1].indexOf('||') === -1) {
+      // "2__6"
       if (Number(answers[singleAnswerCondition[0]]) !== Number(singleAnswerCondition[1])) {
+        // если ответ на второй вопрос не равен тому который мы ищем, то ответ сразу отсеиваем
         return false
       }
     } else {
+      // если несколько ответов "2__6||8||9" 
       const conditionAnswers = singleAnswerCondition[1].split('||')
+      // [6, 8 , 9]
       for (let x = 0; x < conditionAnswers.length; x++) {
+        // если хотя бы один из ответов соотносится с тем, как ответил человек
         if (Number(answers[singleAnswerCondition[0]]) === Number(conditionAnswers[x])) {
           return true
         }
       }
+      // если ни один не найден
       return false
     }
   }
@@ -44,6 +55,7 @@ const checkQuestionValidity = ({ questionId, answers }) => {
       return checkQuestionValidity({ answers, questionId: Number(questionId) + 1 })
     }
   }
+  console.log('here')
   return { ...question, answers: question.answers ? question.answers.filter(answer => checkAnswerValidity({ answers, answer })) : null }
 }
 
